b0fdb462be3ab5ae712447cd5192904cb08757b4
lenninvargas <lennin2666@gmail.com>
2024-12-21 10:21:44 -0500

    add: He agregado la funcion y la vista para actualizar empleo

--- servicios-ya-frontend/src/app/app-routing.module.ts
+++ servicios-ya-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
+import { ActualizarEmpleoComponent } from './pages/actualizar-empleo/actualizar-empleo.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'actualizar-empleo/:id', component: ActualizarEmpleoComponent },
 ];
 
 @NgModule({
