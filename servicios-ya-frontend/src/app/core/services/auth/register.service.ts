0fd34fc456941df0f393b40de270eb8fcf4ca4d7
Jhon Escobedo <jhon040610@gmail.com>
2025-06-23 19:21:03 -0500

    Subiedno modificaciones de Front y Back del registro Usuario (#20)

--- servicios-ya-frontend/src/app/core/services/auth/register.service.ts
+++ servicios-ya-frontend/src/app/core/services/auth/register.service.ts
@@ -9,7 +9,16 @@ import { Observable } from 'rxjs';
 })
 export class RegisterService {
   constructor(private http: HttpClient) {}
-  public register(register: RegisterModel): Observable<any> {
-    return this.http.post<any>(SECURITY_URL + AUTH + REGISTER, register);
+
+  // Método para Empleado
+  public registerEmpleado(dto: RegisterModel): Observable<any> {
+    const url = `${SECURITY_URL}${AUTH}${REGISTER}`;
+    return this.http.post<any>(url, dto);
+  }
+
+  // Método para Empleador
+  public registerEmpleador(dto: RegisterModel): Observable<any> {
+    const url = `${SECURITY_URL}${AUTH}${REGISTER_EMPLEADOR}`;
+    return this.http.post<any>(url, dto);
   }
 }
